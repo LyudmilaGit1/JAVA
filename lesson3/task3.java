69d27cbf409da7d370702e7ace77f86a95dba633
Lyudmila <lyudmila150983@gmail.com>
2023-03-30 17:19:03 +0300

    3

--- lesson3/task3.java
+++ lesson3/task3.java
@@ -1,6 +1,7 @@
 // Реализовать алгоритм сортировки слиянием
 package lesson3;
 import java.util.Arrays;
+import java.util.Random;
 
 public class task3 {
     public static void main(String args[]) {
@@ -15,27 +16,27 @@ public class task3 {
         return result;
     }
 
-    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
-        if (startIndex >= endIndex - 1) {
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int firstIndex, int lastIndex) {
+        if (firstIndex >= lastIndex - 1) {
             return buffer1;
         }
 
-        int middle = startIndex + (endIndex - startIndex) / 2;
-        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
-        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+        int middle = firstIndex + (lastIndex - firstIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, firstIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, lastIndex);
 
-        int index1 = startIndex;
+        int index1 = firstIndex;
         int index2 = middle;
-        int destIndex = startIndex;
+        int destIndex = firstIndex;
         int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
-        while (index1 < middle && index2 < endIndex) {
+        while (index1 < middle && index2 < lastIndex) {
             result[destIndex++] = sorted1[index1] < sorted2[index2]
                     ? sorted1[index1++] : sorted2[index2++];
         }
         while (index1 < middle) {
             result[destIndex++] = sorted1[index1++];
         }
-        while (index2 < endIndex) {
+        while (index2 < lastIndex) {
             result[destIndex++] = sorted2[index2++];
         }
         return result;
