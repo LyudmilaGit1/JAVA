7355786cc8fe909a45043fbe32cf0cf2b6afae08
Lyudmila <lyudmila150983@gmail.com>
2023-03-30 17:09:57 +0300

    2

--- lesson3/task3.java
+++ lesson3/task3.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class task3 {
     public static void main(String args[]) {
-        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        int[] sortArr = {12, 6, 7, 8, 18, 25, 38, 8, 4, 1, 15, 10};
         int[] result = mergeSort(sortArr);
         System.out.println(Arrays.toString(result));
     }
@@ -20,12 +20,10 @@ public class task3 {
             return buffer1;
         }
 
-        //уже отсортирован
         int middle = startIndex + (endIndex - startIndex) / 2;
         int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
         int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
 
-        //слияние
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
